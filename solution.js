a6a2882443addbd2a1d2b0054cb23655235667d7
jessica draper <j.b.draper@hotmail.com>
2018-04-05 14:57:06 +1200

    Solution to challenge complete

--- solution.js
+++ solution.js
@@ -8,8 +8,13 @@ const constantArray = constant.split('')
 function decipher(encryptedText, cipher) {
   let cipherArray = cipher.split('')
   let result = encryptedText.split('').map((character) => {
+    if (!constantArray.includes(character)) {
+      return character
+    }
     let index = cipherArray.indexOf(character)
     return constantArray[index]
   })
   return result.join('')
 }
+
+console.log('the solution to the problem is: ', decipher('knlfgnb, sj koqj o yvnewju', 'oephjizkxdawubnytvfglqsrcm'))
