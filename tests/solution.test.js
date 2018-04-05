00221c9dd56f83e8f6f4700550bddd08edb31d1f
jessica draper <j.b.draper@hotmail.com>
2018-04-05 14:25:56 +1200

    example1 test

--- tests/solution.test.js
+++ tests/solution.test.js
@@ -1,6 +1,14 @@
 const solution = require('../solution')
 
+const constant = 'abcdefghijklmnopqrstuvwxyz'
+
 test('test set up working', () => {
   expect(true).toBeTruthy
+})
+
+test('decipher', () => {
+  let actual = solution.decipher('dzs')
+  let expected = 'car'
+  expect(actual).toBe(expected)
 })
 
