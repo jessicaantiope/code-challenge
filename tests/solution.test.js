161d168f508978370b953250b3bccfc6017a5362
jessica draper <j.b.draper@hotmail.com>
2018-04-05 14:56:03 +1200

    Test for example two

--- tests/solution.test.js
+++ tests/solution.test.js
@@ -6,10 +6,16 @@ test('test set up working', () => {
   expect(true).toBeTruthy
 })
 
-test('decipher', () => {
+test('decipher example One', () => {
   let cipher = 'zodvqukgwefbyitmrsplhacxnj'
   let actual = solution.decipher('dzs', cipher)
   let expected = 'car'
   expect(actual).toBe(expected)
 })
 
+test('decipher example Two', () => {
+  let cipher = 'xipmuzfkbrlwotjancqgveshdy'
+  let actual = solution.decipher('skd qj qucbjvq?', cipher)
+  let expected = 'why so serious?'
+  expect(actual).toBe(expected)
+})
